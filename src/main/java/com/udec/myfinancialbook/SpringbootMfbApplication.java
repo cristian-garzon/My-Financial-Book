dbb6065efed1efb4e47c2f8c320086cb4660ef25
cristian garzon <cristiancamilogarzon2@gmail.com>
2021-05-11 14:38:54 -0500

    repository mfb

---
+++ src/main/java/com/udec/myfinancialbook/SpringbootMfbApplication.java
@@ -0,0 +1,13 @@
+package com.udec.myfinancialbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMfbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMfbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/udec/myfinancialbook/SpringbootMfbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udec.myfinancialbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMfbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
