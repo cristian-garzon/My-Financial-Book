5bafc180c1897629e6c7ba1eb2dffc88665469cf
cristian garzon <cristiancamilogarzon2@gmail.com>
2021-05-21 12:40:46 -0500

    add crossorigin all controllers

--- src/main/java/com/udec/myfinancialbook/controller/BookDayController.java
+++ src/main/java/com/udec/myfinancialbook/controller/BookDayController.java
@@ -15,7 +15,7 @@ import java.util.Date;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins="https://localhost:4200")
+@CrossOrigin(origins="http://localhost:4200")
 @RequestMapping("/BookDay")
 public class BookDayController {
 
